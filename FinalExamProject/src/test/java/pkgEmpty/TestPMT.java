88dfd066f8a3196bcf614737547ce7346f427dc6
emilyevans <emilyevans@wifi-roaming-128-4-110-160.host.udel.edu>
2018-12-13 14:33:31 -0500

    Done!

--- FinalExamProject/src/test/java/pkgEmpty/TestPMT.java
+++ FinalExamProject/src/test/java/pkgEmpty/TestPMT.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgCore.Retirement;
+
 public class TestPMT {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPMT() {
+		
+		Retirement retire = new Retirement(40, 0.07, 20, 0.02, 10000.00,2642.00);
+		double amt = retire.AmountToSave();
+		assertEquals(amt,554.13, 0.01);
+		System.out.println(amt);
+
 	}
 
 }
--- FinalExamProject/src/test/java/pkgEmpty/TestPV.java
+++ FinalExamProject/src/test/java/pkgEmpty/TestPV.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgCore.Retirement;
+
 public class TestPV {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPV() {
+		Retirement retirement = new Retirement(40, 0.07, 20, 0.02, 10000.00,2642.00);
+		double totalAmt = Math.abs(retirement.TotalAmountSaved());
+		assertEquals(totalAmt,1454485.55,0.01);
+		System.out.print(totalAmt);
 	}
 
 }
