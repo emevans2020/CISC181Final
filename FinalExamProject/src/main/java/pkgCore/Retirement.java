6ced9e8b765b02ffd46ba8b64e78af3abf2976f3
emilyevans <emilyevans@wifi-roaming-128-4-110-160.host.udel.edu>
2018-12-13 12:27:09 -0500

    working on it

--- FinalExamProject/src/main/java/pkgCore/Retirement.java
+++ FinalExamProject/src/main/java/pkgCore/Retirement.java
@@ -1,5 +1,7 @@
 package pkgCore;
 
+import org.apache.poi.ss.formula.functions.*;
+
 public class Retirement {
 
 	private int iYearsToWork;
@@ -58,23 +60,26 @@ public class Retirement {
 
 	private double dRequiredIncome;
 	private double dMonthlySSI;
+		
 	
-	//TODO: Build the constructor, getters and setters for the attributes above.
-	
+	/** Determine the amount to save each month based on TotalAmountSaved, YearsToWork
+	and Annual return while working
+		 * 
+		 */
 	public double AmountToSave()
 	{
-		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
-		//		and Annual return while working
 		
-		
-		return 0;
+		double pmt = FinanceLib.pmt(dAnnualReturnWorking/12, iYearsToWork*12, 0, TotalAmountSaved(), false);
+		return pmt;	
 	}
 	
+	/** Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
+		and number of years retired.
+		 * @return
+		 */
 	public double TotalAmountSaved()
 	{
-		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
-		//		and number of years retired.
-		//
-		return 0;
+		double pv = FinanceLib.pv(dAnnualReturnWorking/12, iYearsToWork*12, dRequiredIncome - dMonthlySSI, 0, false);
+		return pv;
 	}
 }
